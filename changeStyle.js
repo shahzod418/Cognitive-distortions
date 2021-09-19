eac4acef4fc7bbba8d7427993c2f667dc8a4c275
Давлатов Шахзод <shahzod418@yandex.ru>
2021-09-20 01:56:59 +0300

    Подготовка проекта

---
+++ changeStyle.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let a = document.createElement("link");
+    a.rel = "stylesheet";
+    a.href = "styles/style-light.css";
+    document.getElementsByTagName("head")[0].appendChild(a);
+}
