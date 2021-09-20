5b1b9134ba3e1ecbdad171134f426740a3f43e27
Давлатов Шахзод <shahzod418@yandex.ru>
2021-09-20 22:19:26 +0300

    Бета версия тёмной версии

--- scripts/changeStyle.js
+++ scripts/changeStyle.js
@@ -2,5 +2,6 @@ window.onload = function() {
     let a = document.createElement("link");
     a.rel = "stylesheet";
     a.href = "styles/style-light.css";
+    a.id = "style-theme";
     document.getElementsByTagName("head")[0].appendChild(a);
 }
