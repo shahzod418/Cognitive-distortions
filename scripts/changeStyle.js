89d3d5b766784e7318a8713ec8de1cf77a9dd559
Давлатов Шахзод <shahzod418@yandex.ru>
2021-09-20 23:10:08 +0300

    Реализована смена темы

--- scripts/changeStyle.js
+++ scripts/changeStyle.js
@@ -1,7 +1,21 @@
+let a;
+
 window.onload = function() {
-    let a = document.createElement("link");
+    a = document.createElement("link");
     a.rel = "stylesheet";
-    a.href = "styles/style-dark.css";
+    a.href = "styles/style-light.css";
     a.id = "style-theme";
     document.getElementsByTagName("head")[0].appendChild(a);
 }
+
+let getTheme = document.getElementById("change-theme");
+getTheme.addEventListener("change", function () {
+    if (document.querySelector(".dark-theme")) {
+        a.href = "styles/style-light.css";
+        getTheme.classList.remove("dark-theme")
+        return;
+    }
+    getTheme.classList.add("dark-theme");
+    a.href = "styles/style-dark.css";
+});
+
